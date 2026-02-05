b5b9b70fad23cc869a20b8a124f9d5ba150ddda7
manmaru001 <it242286@trident.ac.jp>
2026-02-05 10:13:20 +0900

    Prevention of sound cracking

--- Assets/Script/PlayerContoroller.cs
+++ Assets/Script/PlayerContoroller.cs
@@ -52,6 +52,10 @@ public class PlayerController : MonoBehaviour
     // 内部：アニメ coroutine 管理
     Coroutine digAnimCoroutine = null;
 
+    [Header("Audio Settings")]
+    public float digSoundInterval = 0.1f; // 0.1秒間隔以内なら鳴らさない
+    private float lastDigSoundTime = 0f;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -93,11 +97,24 @@ public class PlayerController : MonoBehaviour
             // タイルが一つでも壊れたら効果音を鳴らす
             if (removed > 0)
             {
-                if (SoundManager != null)
+                // 音の重複による音割れ防止
+                if (SoundManager != null && Time.time - lastDigSoundTime > digSoundInterval)
                 {
                     var sm = SoundManager.GetComponent<SoundManager>();
-                    if (sm != null) sm.PlaySFX("Sound_Dig");
+                    if (sm != null)
+                    {
+                        // ピッチを変更して再生する
+                        float randomPitch = Random.Range(0.8f, 1.2f);
+                        sm.PlaySFX("Sound_Dig", 1.0f, randomPitch);
+
+                        lastDigSoundTime = Time.time; // 再生時間を更新
+                    }
                 }
+                //if (SoundManager != null)
+                //{
+                //    var sm = SoundManager.GetComponent<SoundManager>();
+                //    if (sm != null) sm.PlaySFX("Sound_Dig");
+                //}
             }
 
             // アニメーション：掘削中フラグを短時間 true にする
